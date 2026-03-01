f7a4294a7c2c9550adfbd111da4fd4d80a496e2d
Atomic645 <roumyajitsardar@gmail.com>
2026-03-01 15:24:28 +0530

    Add button click event to display AI suggestion

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("button").addEventListener("click", () => {
+  document.querySelector(".output").innerText =
+    "AI suggests prioritizing this task and completing it during peak focus hours.";
+});
